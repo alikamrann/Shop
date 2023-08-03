99a21bd1f6e995ad94ca98b39169b1d0e25f4646
ali kamran <ali.kamran.98.98@gmail.com>
2023-08-03 04:38:51 +0330

    first commit

--- app/src/main/java/com/alikamran/shop/MainScreen.kt
+++ app/src/main/java/com/alikamran/shop/MainScreen.kt
@@ -4,19 +4,16 @@ import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.navigation.NavController
 import androidx.navigation.NavDestination
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.ComposeNavigator
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 
@@ -62,7 +59,7 @@ fun RowScope.AddItem(
     currentDestination: NavDestination?,
     navController: NavHostController
 ){
-    //اینجا  میشه  آیتم ها رو کاستوم کرد
+    //اینجا  میشه   آیتم ها رو کاستوم کرد
     NavigationBarItem(label = {
         Text(text = screen.title)
     },
